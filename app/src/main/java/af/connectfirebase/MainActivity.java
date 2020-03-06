3c5c6c0a95f9c881e067e416621554dd97963939
YasasDEK <ydilshan.ek@gmail.com>
2020-03-06 20:03:07 +0530

    auto increment index

--- app/src/main/java/af/connectfirebase/MainActivity.java
+++ app/src/main/java/af/connectfirebase/MainActivity.java
@@ -1,5 +1,6 @@
 package af.connectfirebase;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +16,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 EditText firstname, mobile, email;
-Button button;
+Button button, button2;
 DatabaseReference reff;
 Member member;
     @Override
@@ -40,5 +41,19 @@ Member member;
                 Toast.makeText(MainActivity.this,"data inserted successfully", Toast.LENGTH_LONG).show();
             }
         });
-    }
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity();
+                }
+            });
+        }
+
+        public void openActivity() {
+            Intent intent = new Intent(this, Main2Activity.class);
+            startActivity(intent);
+        }
+
 }
